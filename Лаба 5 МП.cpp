19707992459b94a65707074922a51a66cdfb138f
Parcif <arteminkin@yandex.ru>
2022-12-04 21:08:51 +0400

    Навел порядок и оптимизировал код

--- Лаба 5 МП.cpp
+++ Лаба 5 МП.cpp
@@ -22,7 +22,6 @@ private:
 	int days[12] = { 31,29,31,30,31,30,31,31,30,31,30,31 };
 	string check_date()
 	{
-		cout << date;
 		string inp;
 		int day;
 		int month;
@@ -34,18 +33,14 @@ private:
 			getline(cin, inp);
 			try
 			{
-				double check = stod(inp, &ptr);
-
 				if (inp.size() != 5 || inp[2] != '.')
-					throw runtime_error("\nError!!! Repeat the input!\n");
+					throw runtime_error("\nОшибка!!! Повторите ввод: ");
+
+				double check = stod(inp, &ptr);
 
 				regex regex("\\.");
 
 				vector<string> out(sregex_token_iterator(inp.begin(), inp.end(), regex, -1), sregex_token_iterator());
-
-				/*for (auto& inp : out) {
-					std::cout << inp << std::endl;
-				}*/
 
 				day = stoi(out[0]);
 				month = stoi(out[1]);
@@ -56,18 +51,20 @@ private:
 					return inp;
 				}
 				else
-					cout << "\nError! Repeat the input!\n";
+					cout << "\nОшибка! Повторите ввод: ";
 
 			}
 			catch (invalid_argument)
 			{
-				cout << "\nError!!! Repeat the input!\n";
+				cout << "\nОшибка!!! Повторите ввод: ";
 			}
 			catch (runtime_error& e)
 			{
 				cout << e.what() << endl;
 			}
+
 		}
+
 	}
 
 	// Проверка ввода давления
@@ -133,15 +130,7 @@ public:
 		pr3 = check_press();
 		press3 = pr3;
 
-		averagepressure();
 	}
-
-	//tirepressure(vector<double> vec)  // Считывающий из файла давление конструктор
-	//{
-	//	press1 = vec[0];
-	//	press2 = vec[1];
-	//	press3 = vec[2];
-	//}
 
 	tirepressure(string d, vector<double> vec) // Считывающий из файла конструктор
 	{
@@ -156,37 +145,6 @@ public:
 };
 
 
-int check_n(int min, string strln) // кол - во эл тов
-{
-	int res;
-	string inp;
-	bool state = true;
-	cout << strln << endl;
-
-	while (state)
-	{
-		getline(cin, inp);
-		try
-		{
-			res = stoi(inp);
-			if (inp.size() == to_string(res).size() && res > min)
-			{
-				state = false;
-			}
-			else
-				cout << "\nОшибка! Повторите ввод!\n";
-
-		}
-		catch (invalid_argument)
-		{
-			cout << "\nОшибка! Повторите ввод!\n";
-		}
-
-	}
-	return res;
-}
-
-
 char check_f_i() // f и i
 {
 	string inp;
@@ -195,181 +153,18 @@ char check_f_i() // f и i
 	while (state)
 	{
 		getline(cin, inp);
-		try
-		{
-			if (inp.size() == 1 && (inp[0] == 'f' || inp[0] == 'i'))
-			{
-				state = false;
-			}
-			else
-				cout << "\nОшибка! Повторите ввод!\n";
-
-		}
-		catch (invalid_argument)
-		{
-			cout << "\nОшибка! Повторите ввод!\n";
-		}
-
-	}
-	return inp[0];
-}
-
-
-char check_r_c() // r и c
-{
-	string inp;
-	bool state = true;
-
-	while (state)
-	{
-		getline(cin, inp);
-		try
-		{
-			if (inp.size() == 1 && (inp[0] == 'r' || inp[0] == 'c'))
-			{
-				state = false;
-				
-			}
-			else
-				cout << "\nОшибка! Повторите ввод!\n";
-
-		}
-		catch (invalid_argument)
-		{
-			cout << "\nОшибка! Повторите ввод!\n";
-		}
-
-	}
-	return inp[0];
-}
-
-
-void consOutput(vector<tirepressure>& vec, vector<double>& proc)  // Вывод массивов в консоль
-{
-	cout << "\nМассив объектов класса: [ ";
-	
-	for (int i = 0; i < vec.size(); i++)
-	{
-		cout << vec[i];
-	}
-	cout << "]" << endl;
-
-	cout << "\nМассив результатов обработки: [ ";
-	for (int i = 0; i < proc.size(); i++)
-	{
-		cout << proc[i] << " ";
-	}
-	cout << "]" << endl;
-
-}
-
-void fOutput(vector<tirepressure>& vec, vector<double>& proc)  // Вывод массивов в файл
-{
-
-	ofstream file; // создаем объект класса ofstream
-
-	file.open("C:\\Users\\artem\\OneDrive\\Рабочий стол\\Лаба 5 МП записываем.txt");
-
-	
-	if (file.is_open())  // Проверка отрылся файл или нет
-		cout << "\n\nВсе хорошо! Файл открыт! Записано в файл!\n" << endl;
-	else
-	{
-		cout << "\nФайл не открыт!";
-		throw runtime_error("\nЧто то пошло не так!\n");
-	}
-
-	file << "Массив объектов класса: [ ";
-	for (int i = 0; i < vec.size(); i++)
-	{
-		file << vec[i];
-	}
-	file << "]" << endl;
-
-	file << "\nМассив результатов обработки: [ ";
-	for (int i = 0; i < proc.size(); i++)
-	{
-		file << proc[i] << " ";
-	}
-	file << "]" << endl;
-
-	file.close();
-
-}
-
-ostream& operator<<(ostream& os, const tirepressure& point)
-{
-	os << point.date << " " << point.press1 << " " << point.press2 << " " << point.press3 << " ";
-	return os;
-}
-
-istream& operator>>(istream& is, tirepressure& point)
-{
-	is >> point.date >> point.press1 >> point.press2 >> point.press3;
-	return is;
-}
-
-double check_fileInp(string& str)
-{
-	double res;
-	size_t ptr = 0;
-	
-	res = stod(str, &ptr);
-	if ((res <= 0) && (str.size() != ptr))
-	{
-		throw runtime_error("\nОшибка! Некорректные данные в файле!\n");
-	}
-	else
-	{
 		
-		return res;
-	}
-	
-}
-
-void check_fileInp2(string str, vector<double>& digits)
-{	
-	size_t ptr1 = 0; size_t ptr2 = 0; size_t ptr3 = 0;
-	
-	double press1;
-	double press2;
-	double press3;
-
-	bool state = true;
-
-	while (state)
-	{
-		if (isdigit(str[0]) == 0)									// выбрасываем ошибку если первый элемент не цифра
-			throw runtime_error("\nError!!! Repeat the input!\n");
-
-		regex regex("\\ ");
-
-		vector<string> out(sregex_token_iterator(str.begin(), str.end(), regex, -1), sregex_token_iterator());
-
-		/*for (auto& inp : out) {
-			std::cout << inp << std::endl;
-		}*/
-
-		press1 = stod(out[0], &ptr1);
-		press2 = stod(out[1], &ptr2);
-		press3 = stod(out[2], &ptr3);
-
-		
-		if (((press1 > 0) && (out[0].size() == ptr1) && (press2 > 0) && (out[1].size() == ptr2) && (press3 > 0) && (out[2].size() == ptr3)))
+		if (inp.size() == 1 && (inp[0] == 'f' || inp[0] == 'i'))
 		{
 			state = false;
-			digits.push_back(press1); 
-			digits.push_back(press2); 
-			digits.push_back(press3);
 		}
 		else
-		{
-			throw runtime_error("\nНекорректные данные в файле!\n");
-		}
-		
-	}
+			cout << "\nОшибка! Повторите ввод: ";
 
+	}
+	return inp[0];
 }
+
 
 void dateFileInput(string& str, string& date)
 {
@@ -377,6 +172,44 @@ void dateFileInput(string& str, string& date)
 	date = (n != string::npos) ? str.substr(0, n) : "";
 
 	str.erase(0, 6);
+}
+
+void check_fileInp(string str, vector<double>& digits)
+{
+	size_t ptr1 = 0; size_t ptr2 = 0; size_t ptr3 = 0;
+
+	double press1;
+	double press2;
+	double press3;
+	bool state = true;
+
+	while (state)
+	{
+		if (isdigit(str[0]) == 0)									// выбрасываем ошибку если первый элемент не цифра
+			throw runtime_error("\nНекорректные данные в файле!\n");
+
+		regex regex("\\ ");
+
+		vector<string> out(sregex_token_iterator(str.begin(), str.end(), regex, -1), sregex_token_iterator());
+
+		press1 = stod(out[0], &ptr1);
+		press2 = stod(out[1], &ptr2);
+		press3 = stod(out[2], &ptr3);
+
+		if ((press1 > 0) && (out[0].size() == ptr1) && (press2 > 0) && (out[1].size() == ptr2) && (press3 > 0) && (out[2].size() == ptr3))
+		{
+			state = false;
+			digits.push_back(press1);
+			digits.push_back(press2);
+			digits.push_back(press3);
+		}
+		else
+		{
+			throw runtime_error("\nНекорректные данные в файле!\n");
+		}
+
+	}
+
 }
 
 void fInput(vector<tirepressure>& obj, vector<double>& proc)  // Чтение из файла
@@ -392,46 +225,74 @@ void fInput(vector<tirepressure>& obj, vector<double>& proc)  // Чтение и
 		throw runtime_error("\nЧто то пошло не так!\n");
 	}
 
-	bool state2 = true;
-	
-	while (state2)
+	bool state = true;
+
+	while (state)
 	{
-		
 		string str;
 		getline(file, str);
-
-		// Дата:
-
+		
 		string date;
 		dateFileInput(str, date);
-		//tirepressure d(date);
-		//obj.push_back(d);
-
-		// Давление:
 
 		vector<double> digits;
-		check_fileInp2(str, digits);
+		check_fileInp(str, digits);
 		tirepressure p(date, digits);
 		obj.push_back(p);
 
 		if (file.eof())
 		{
-			state2 = false;
+			state = false;
 		}
 	}
+
+	file.close();
 
 	for (int i = 0; i < obj.size(); i++)
 	{
 		proc.push_back(obj[i].averagepressure());
 	}
 
-	file.close();	
-
 }
 
-void consInput(int& n, vector<tirepressure>& obj, vector<double>& proc)
+
+int check_n(string str) // кол - во эл тов
 {
-	n = check_n(0, "\nВведите количество элементов массива: ");
+	cout << str;
+	int res;
+	string inp;
+	bool state = true;
+
+	while (state)
+	{
+		getline(cin, inp);
+		try
+		{
+			res = stoi(inp);
+			if (inp.size() == to_string(res).size() && res > 0)
+			{
+				state = false;
+			}
+			else
+				cout << "\nОшибка! Повторите ввод: ";
+
+		}
+		catch (invalid_argument)
+		{
+			cout << "\nОшибка! Повторите ввод: ";
+		}
+		catch (out_of_range)
+		{
+			cout << "\nО нет, выход за пределы максимально возможного значения!!! Повторите ввод: ";
+		}
+
+	}
+	return res;
+}
+
+void consInput(vector<tirepressure>& obj, vector<double>& proc)
+{
+	int n = check_n("\nВведите количество элементов массива: ");
 
 	cout << "\nВведите объекты класса: " << endl;
 
@@ -448,16 +309,101 @@ void consInput(int& n, vector<tirepressure>& obj, vector<double>& proc)
 
 }
 
+
+char check_r_c() // r и c
+{
+	string inp;
+	bool state = true;
+
+	while (state)
+	{
+		getline(cin, inp);
+
+		if (inp.size() == 1 && (inp[0] == 'r' || inp[0] == 'c'))
+		{
+			state = false;
+		}
+		else
+			cout << "\nОшибка! Повторите ввод: ";
+
+	}
+	return inp[0];
+}
+
+
+void fOutput(vector<tirepressure> obj, vector<double> proc)  // Вывод массивов в файл
+{
+	ofstream file; // создаем объект класса ofstream
+	file.open("C:\\Users\\Artem1\\Desktop\\Лаба 5 МП записываем.txt");
+
+	if (file.is_open())  // Проверка отрылся файл или нет
+		cout << "\n\nВсе хорошо! Файл открыт! Записано в файл!\n" << endl;
+	else
+	{
+		cout << "\nФайл не открыт!";
+		throw runtime_error("\nЧто то пошло не так!\n");
+	}
+
+	file << "Массив объектов класса: [ ";
+	for (int i = 0; i < obj.size(); i++)
+	{
+		file << obj[i];	 // перегрузка вывода
+	}
+	file << "]" << endl;
+
+	file << "\nМассив результатов обработки: [ ";
+	for (int i = 0; i < proc.size(); i++)
+	{
+		file << proc[i] << " ";
+	}
+	file << "]" << endl;
+
+	file.close();
+
+}
+
+void consOutput(vector<tirepressure> obj, vector<double> proc)  // Вывод массивов в консоль
+{
+	cout << "\nМассив объектов класса: [ ";
+	
+	for (int i = 0; i < obj.size(); i++)
+	{
+		cout << obj[i];	 // перегрузка вывода
+	}
+	cout << "]" << endl;
+
+	cout << "\nМассив результатов обработки: [ ";
+	for (int i = 0; i < proc.size(); i++)
+	{
+		cout << proc[i] << " ";
+	}
+	cout << "]" << endl;
+
+}
+
+
+ostream& operator<<(ostream& os, const tirepressure& point)	 // перегрузка вывода
+{
+	os << point.date << " " << point.press1 << " " << point.press2 << " " << point.press3 << " ";
+	return os;
+}
+
+istream& operator>>(istream& is, tirepressure& point)	 // перегрузка ввода
+{
+	is >> point.date >> point.press1 >> point.press2 >> point.press3;
+	return is;
+}
+
+
 int main()
 {
 	setlocale(LC_ALL, "ru");
 	setlocale(LC_NUMERIC, "eng");
 
-	int n;
 	vector<tirepressure> obj;
 	vector<double> proc;
 
-	cout << "\nВведите f, если хотите прочитать из файла, или i, если хотите ввести вручную!" << endl;
+	cout << "\nВведите f, если хотите прочитать из файла, или i, если хотите ввести вручную: ";
 	char v = check_f_i();
 
 	try
@@ -468,11 +414,11 @@ int main()
 		}
 		else if (v == 'i')  // РУЧНОЙ ВВОД
 		{
-			consInput(n, obj, proc);
+			consInput(obj, proc);
 		}
 
 
-		cout << "\nВведите r, если хотите записать в файл, или c, если хотите вывести в консоль!" << endl;
+		cout << "\nВведите r, если хотите записать в файл, или c, если хотите вывести в консоль: ";
 		char ans = check_r_c();
 
 		if (ans == 'r')  // ЗАПИСЬ В ФАЙЛ
@@ -487,10 +433,6 @@ int main()
 	catch (runtime_error& e)
 	{
 		cout << e.what();
-	}
-	catch (invalid_argument)
-	{
-		cout << "\nОшибка! Некорректные данные в файле!\n";
 	}
 
 }
